8f238fe4afad52547651b070b637b08b41c0cc63
Alexey Borodov <dojdiq1@gmail.com>
2023-03-14 17:06:18 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,7 @@
+valid_email = "u3bhbflxt3@kzccv.com"
+valid_password = "qwerty21"
+
+invalid_email = 'booty775@mail.com'
+invalid_password = 'tryin631'
+
+auth_key = "a0d169f2bdd827cd839bbf3604339e9ca8143b6f994d2fce80ea2389"
